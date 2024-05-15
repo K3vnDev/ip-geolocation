adbe78e7998f7ec47bf2d8dadc00d9bf1502a32d
K3vnDev <158790560+K3vnDev@users.noreply.github.com>
2024-05-15 16:35:28 -0500

    fix marker bug

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,14 @@
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+import markerIcon from '../assets/marker-icon-2x.png'
+import L from 'leaflet'
 
 export function Map ({ position }) {
+  const icon = L.icon({
+    iconUrl: markerIcon,
+    iconSize: [20]
+  })
+
   return (
     <MapContainer
       className='map-wrapper'
@@ -19,7 +26,7 @@ export function Map ({ position }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={position} />
+      <Marker position={position} icon={icon} />
     </MapContainer>
   )
 }
